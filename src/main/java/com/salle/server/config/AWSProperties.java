60391277d27de1e67a040ff18a90da86de1ea7b8
baekho <klyhyeon@gmail.com>
2022-05-10 19:57:33 +0900

    feat: add AWS S3 endpointUrl value and change method

--- src/main/java/com/salle/server/config/AWSProperties.java
+++ src/main/java/com/salle/server/config/AWSProperties.java
@@ -1,23 +1,29 @@
 package com.salle.server.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
-@ConfigurationProperties(prefix = "aws")
+@ConfigurationProperties(prefix = "cloud.aws")
 @ConstructorBinding
 @Getter
-@RequiredArgsConstructor
 public class AWSProperties {
 
     private final S3 s3;
 
+    public AWSProperties(S3 s3) {
+        this.s3 = s3;
+    }
+
     @Getter
-    @RequiredArgsConstructor
     public static class S3 {
 
         private final String bucket;
+        private final String endpointUrl;
 
+        public S3(String bucket, String endpointUrl) {
+            this.bucket = bucket;
+            this.endpointUrl = endpointUrl;
+        }
     }
 }
--- src/main/java/com/salle/server/utils/AmazonS3Service.java
+++ src/main/java/com/salle/server/utils/AmazonS3Service.java
@@ -1,23 +1,25 @@
 package com.salle.server.utils;
 
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AbstractAmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.salle.server.config.AWSProperties;
-import org.springframework.beans.factory.annotation.Value;
+import com.salle.server.domain.enumeration.ErrorCode;
+import com.salle.server.error.SlErrorException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AmazonS3Service {
 
-    private AmazonS3 amazonS3;
-
+    private AbstractAmazonS3 amazonS3;
     private final AWSProperties awsProperties;
 
     public AmazonS3Service(AWSProperties awsProperties) {
@@ -30,8 +32,31 @@ public class AmazonS3Service {
     }
 
 
-    public String uploadImg(String bucket, String fileName, MultipartFile multiFile) throws IOException {
-        File uploadFile = convert(multiFile);
+    public List<String> convertToFileAndUpload(List<MultipartFile> multipartFiles) {
+        String bucketName = awsProperties.getS3().getBucket();
+        String endPointUrl = awsProperties.getS3().getEndpointUrl();
+        String urlPrefix = endPointUrl + "/" + bucketName + "/";
+        List<String> fileUrls = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            File uploadFile = convertMultiPartToFile(multipartFile);
+            upholdFileToS3(bucketName, uploadFile);
+            String uploadedFileUrl = urlPrefix + uploadFile.getName();
+            fileUrls.add(uploadedFileUrl);
+        }
+        return fileUrls;
+    }
+
+    private void upholdFileToS3(String bucketName, File uploadFile) {
+        amazonS3.putObject(new PutObjectRequest(
+                bucketName,
+                uploadFile.getName(),
+                uploadFile
+        )
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String convertToFileAndUpload(String bucket, String fileName, MultipartFile multiFile) throws IOException {
+        File uploadFile = convertMultiPartToFile(multiFile);
         amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
         return "uploadS3 success";
     }
@@ -41,12 +66,14 @@ public class AmazonS3Service {
         return "uploadS3 success";
     }
 
-
-    public File convert(MultipartFile multiFile) throws IOException {
-        File uploadFile = new File(multiFile.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(uploadFile);
-        fos.write(multiFile.getBytes());
-        fos.close();
+    public File convertMultiPartToFile(MultipartFile multiFile) {
+        String multipartFileName = multiFile.getOriginalFilename();
+        File uploadFile = new File(multipartFileName);
+        try (FileOutputStream fos = new FileOutputStream(uploadFile)) {
+            fos.write(multiFile.getBytes());
+        } catch (IOException e) {
+            throw new SlErrorException(ErrorCode.FILE_UPLOAD_ERROR);
+        }
         return uploadFile;
     }
 
