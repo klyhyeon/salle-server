391555dc6d6460247147c79713ec588136121ed8
baekho <klyhyeon@gmail.com>
2022-05-11 22:26:08 +0900

    feat: add error code for product comment

--- src/main/java/com/salle/server/domain/enumeration/ErrorCode.java
+++ src/main/java/com/salle/server/domain/enumeration/ErrorCode.java
@@ -15,7 +15,10 @@ public enum ErrorCode {
     FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 오류가 발생했습니다."),
 
     //PRODUCT
-    PRODUCT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "존재하지 않는 상품입니다.");
+    PRODUCT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "존재하지 않는 상품입니다."),
+
+    //PRODUCT COMMENT
+    PRODUCT_COMMENT_NOT_FOUND(HttpStatus.UNAUTHORIZED, "존재하지 않는 댓글입니다.");
 
     private HttpStatus errorHttpStatus;
     private String errorMessage;
