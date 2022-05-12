1b6121c6b71a6aa9fb96fe3111a450096a283315
baekho <klyhyeon@gmail.com>
2022-05-12 17:01:17 +0900

    test: add lazy, eager fetch test

--- src/test/java/com/salle/server/repository/ProductRepositoryTest.java
+++ src/test/java/com/salle/server/repository/ProductRepositoryTest.java
@@ -1,15 +1,23 @@
 package com.salle.server.repository;
 
 import com.salle.server.config.QueryDSLConfig;
+import com.salle.server.domain.dto.ProductDTO;
 import com.salle.server.domain.entity.Product;
+import com.salle.server.domain.entity.ProductComment;
+import com.salle.server.domain.entity.ProductNestedComment;
 import com.salle.server.domain.enumeration.ProductStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,13 +29,57 @@ class ProductRepositoryTest {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ProductCommentRepository productCommentRepository;
+    @Autowired
+    private ProductNestedCommentRepository productNestedCommentRepository;
+    @Autowired
+    private EntityManager entityManager;
+
+    private ModelMapper modelMapper = new ModelMapper();
 
     @Test
     @DisplayName("Product Save 테스트")
     void productSaveTest() {
         Product product = new Product();
         product.setStatus(ProductStatus.SELL);
-        Product savedProduct = productRepository.save(product);
-        assertThat(savedProduct.getStatus()).isEqualTo(ProductStatus.SELL);
+        productRepository.save(product);
+    }
+
+    @Test
+    @DisplayName("Product lazy fetch 테스트")
+    void productLazyFetchTest() {
+        productRepository.findById(1L);
+    }
+
+    @Test
+    @DisplayName("Product eager fetch 테스트")
+    void productEagerFetchTest() {
+        List<Product> allProducts = productRepository.findAll();
+        ProductComment productComment = ProductComment.getRawInstance(allProducts.get(0), null);
+        productComment.setContent("잘 읽고 갑니다.");
+        productCommentRepository.save(productComment);
+        List<ProductDTO> productDto =
+                allProducts.stream().map(product -> modelMapper.map(product, ProductDTO.class)).collect(Collectors.toList());
+        assertThat(productDto.get(0).getProductComments().get(0).getContent()).contains("잘 읽고 갑니다.");
+    }
+
+    @Test
+    @DisplayName("ProductComment cascade 테스트")
+    void productCommentCascadeTest() {
+        Product product = productRepository.findAll().get(0);
+
+        ProductComment productComment = ProductComment.getRawInstance(product, null);
+        productComment.setContent("잘 읽고 갑니다.");
+        productCommentRepository.save(productComment);
+        entityManager.flush();
+
+        ProductNestedComment productNestedComment = ProductNestedComment.getInstance(product, null);
+        ProductComment newProductComment = productCommentRepository.findAll().get(0);
+        newProductComment.addProductNestedComment(productNestedComment);
+        productCommentRepository.save(newProductComment);
+
+        productCommentRepository.delete(productComment);
+        entityManager.flush();
     }
 }
